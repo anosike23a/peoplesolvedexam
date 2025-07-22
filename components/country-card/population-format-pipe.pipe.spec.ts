87bd0f62ac47f72d39131b6bae78a58590b3c3d8
anosike23a <anosike23a@gmail.com>
2025-07-23 00:05:34 +0200

    Delete components directory

--- components/country-card/population-format-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PopulationFormatPipePipe } from './population-format-pipe.pipe';
-
-describe('PopulationFormatPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PopulationFormatPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
